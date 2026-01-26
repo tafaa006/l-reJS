065e197eb30ecff19a926290b51c7fb799130ede
tafaa006 <tafaa006@osloskolen.no>
2026-01-26 11:57:14 +0100

    gayhomseklubb

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("---Oppgave2---")
+console.log("1.", 8+"4");
+console.log("2.", 8*"4");
+console.log("3.", 8/"4");
+console.log("4.", 8-"4");
+
+console.log("---Oppgave3---")
+let a = 3;
+let b = 7;
+
+console.log("1.", a*b)
+console.log("2.", a*(b-1))
+console.log("3.", (a-1)*b)
+console.log("4.", a*(b+1))
+console.log("5.", a-b)
+
+console.log("---Oppgave4---")
+
+let timer = 4;
+let minutter = 27;
+let sekunder = 33;
+
+console.log(timer*3600+minutter*60+sekunder, "sekunder i", timer,"timer", minutter,"minutter og", sekunder,"sekunder")
+
+console.log("---Oppgave5---")
+
+let SEKUNDER = 5790
+let MINUTTER = SEKUNDER/60
+let TIMER = MINUTTER/60
+
+console.log("sekunder =", SEKUNDER, "minutter =", MINUTTER, "timer =", TIMER)
+
+console.log("---Oppgave6---")
+
+let alfabet = "abcdefghijklmnopqrstvuwxyzøæå";
+console.log(alfabet.charAt(7), alfabet.charAt(0), alfabet.charAt(11), alfabet.charAt(11), alfabet.charAt(14))
+
+console.log("---Oppgave7---")
+
+if(67 >= 31){
+    console.log("67 er størst")
+}
